8bfbd11393be13a6cfe4818af693b77b2dde9551
Muchezz <muchezz@outlook.com>
2021-11-12 18:00:00 +0300

    Added Image

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ set -e
 REGION="us-west-2"
 ECR_URL="910756530301.dkr.ecr.us-west-2.amazonaws.com/L"
 BUILD_NUMBER="${BUILD_NUMBER:-$(date +%s)}"
-JAR_PATH=Dockerfile
+# JAR_PATH=Dockerfile
 
 echo "Building $BUILD_NUMBER" 
 
@@ -12,7 +12,7 @@ echo "Building $BUILD_NUMBER"
 $(aws ecr get-login --no-include-email --region $REGION)
 # build a docker image with a unique build number
 docker build -t $ECR_URL:$BUILD_NUMBER \
-  --build-arg JAR_PATH="$JAR_PATH" \
+#   --build-arg JAR_PATH="$JAR_PATH" \
   --build-arg VERSION="$BUILD_NUMBER" \
   .
 
